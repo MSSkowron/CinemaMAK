b8793e95b0c1256f04f717e2bf564fbd988eb82e
Ch-Adrian <aderter116@gmail.com>
2022-12-02 22:58:04 +0100

    Email validation

--- src/main/java/pl/edu/agh/cs/to/cinemamak/controllers/LoginController.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/controllers/LoginController.java
@@ -2,9 +2,18 @@ package pl.edu.agh.cs.to.cinemamak.controllers;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.stereotype.Component;
+import pl.edu.agh.cs.to.cinemamak.JavaFxApplication;
+import pl.edu.agh.cs.to.cinemamak.repositories.RoleRepository;
+import pl.edu.agh.cs.to.cinemamak.repositories.UserRepository;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -23,6 +32,9 @@ public class LoginController {
     @FXML
     private Button buttonRegister;
 
+    public LoginController(){
+    }
+
     @FXML
     private void onButtonExit(){
         Platform.exit();
@@ -30,12 +42,27 @@ public class LoginController {
 
     @FXML
     private void onButtonLogin(){
-        System.out.println("Login");
+
+        String email = this.textFieldEmail.getCharacters().toString();
+        String password = this.textFieldPassword.getCharacters().toString();
+
+        if(EmailValidator.getInstance().isValid(email)){
+            System.out.println("Email is valid");
+        }
     }
 
     @FXML
     private void onButtonRegister(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(JavaFxApplication.class.getResource("login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 616, 433);
+//            stage.setTitle("Cinema Application");
+//            stage.setScene(scene);
+//            stage.show();
 
+        } catch( IOException exception){
+            exception.getStackTrace();
+        }
     }
 
 }
--- src/main/java/pl/edu/agh/cs/to/cinemamak/controllers/RegisterController.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/controllers/RegisterController.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class RegisterController {
 
@@ -32,7 +33,14 @@ public class RegisterController {
 
     @FXML
     private void onButtonRegisterClick(){
+        String firstName = this.textFieldFirstName.getCharacters().toString();
+        String lastName = this.textFieldLastName.getCharacters().toString();
+        String email = this.textFieldEmail.getCharacters().toString();
+        String password = this.textFieldPassword.getCharacters().toString();
 
+        if(EmailValidator.getInstance().isValid(email)){
+            System.out.println("Email is valid");
+        }
     }
 
 }
