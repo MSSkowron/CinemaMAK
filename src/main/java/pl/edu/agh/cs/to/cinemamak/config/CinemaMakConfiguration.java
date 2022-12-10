d9ef7a3f760c339d42d92af2e9609f6d53d2b772
Mateusz Skowron <405743@student.agh.edu.pl>
2022-12-11 00:56:44 +0100

    Refactor Configuration

--- src/main/java/pl/edu/agh/cs/to/cinemamak/config/CinemaMakConfiguration.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/config/CinemaMakConfiguration.java
@@ -1,9 +1,7 @@
 package pl.edu.agh.cs.to.cinemamak.config;
 
-import javafx.stage.Stage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,11 +10,5 @@ public class CinemaMakConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    @Lazy()
-    public Stage primaryStage(Stage stage) {
-        return stage;
     }
 }
