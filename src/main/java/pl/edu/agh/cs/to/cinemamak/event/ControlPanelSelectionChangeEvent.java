5bdeb5f4de448037bd146b55b142a3b867f99a20
Mateusz Skowron <405743@student.agh.edu.pl>
2022-12-11 00:57:23 +0100

    Refactor ControlPanelSelectionChangeEvent

--- src/main/java/pl/edu/agh/cs/to/cinemamak/event/ControlPanelSelectionChangeEvent.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/event/ControlPanelSelectionChangeEvent.java
@@ -3,7 +3,6 @@ package pl.edu.agh.cs.to.cinemamak.event;
 import org.springframework.context.ApplicationEvent;
 
 public class ControlPanelSelectionChangeEvent extends ApplicationEvent {
-
     public ControlPanelSelectionChangeEvent(Object source) {
         super(source);
     }
