d3bf772334cf6428e6e8238cac32e01bfdaf8b94
Mateusz Skowron <405743@student.agh.edu.pl>
2022-12-11 00:42:33 +0100

    Refactor AdminController

--- src/main/java/pl/edu/agh/cs/to/cinemamak/controller/AdminController.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/controller/AdminController.java
@@ -19,71 +19,51 @@ import pl.edu.agh.cs.to.cinemamak.service.UserService;
 @Component
 @FxmlView("admin-view.fxml")
 public class AdminController implements ApplicationListener<ControlPanelSelectionChangeEvent> {
-
     @FXML
     private ListView<User> usersListView;
     @FXML
     private ChoiceBox<String> roleChoiceBox;
-
     @FXML
     private Button roleSetButton;
-
     @FXML
     private Button exitButton;
-
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
-
     private Stage stage;
+    private final UserService userService;
+    private final SessionService sessionService;
+    private final FxWeaver fxWeaver;
 
-    private UserService userService;
-    private SessionService sessionService;
-    private FxWeaver fxWeaver;
-
-    public AdminController(UserService userService,
-                           SessionService sessionService,
-                           FxWeaver fxWeaver){
-
+    public AdminController(UserService userService, SessionService sessionService, FxWeaver fxWeaver) {
         this.userService = userService;
         this.sessionService = sessionService;
         this.fxWeaver = fxWeaver;
-
     }
 
-    public void  initialize(){
-
+    public void  initialize() {
         this.roleChoiceBox.getItems().add((RoleName.Admin.toString()));
         this.roleChoiceBox.getItems().add((RoleName.Manager.toString()));
         this.roleChoiceBox.getItems().add((RoleName.Employee.toString()));
 
-        userService.getUsers().ifPresent( listU ->{
+        userService.getUsers().ifPresent( listU -> listU.forEach(user -> this.usersListView.getItems().add(user)));
 
-            listU.forEach(user ->{
-                this.usersListView.getItems().add(user);
-            });
-
-        });
-
-        this.usersListView.setCellFactory(param -> new ListCell<User>(){
+        this.usersListView.setCellFactory(param -> new ListCell<>() {
             @Override
-            protected void updateItem(User item, boolean empty){
+            protected void updateItem(User item, boolean empty) {
                 super.updateItem(item, empty);
-                if(empty){
+                if (empty) {
                     this.setText(null);
-                }
-                else{
-                    this.setText(item.getFirstName()+" "+item.getLastName()+"\n"+(item.getEmailAddress())+"\n"+item.getRole().getRoleName().toString());
+                } else {
+                    this.setText(item.getFirstName() + " " + item.getLastName() + "\n" + (item.getEmailAddress()) + "\n" + item.getRole().getRoleName().toString());
                 }
             }
         });
 
-        this.usersListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->{
-            applicationEventPublisher.publishEvent(new ControlPanelSelectionChangeEvent(this));
-        });
-
+        this.usersListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue)
+                -> applicationEventPublisher.publishEvent(new ControlPanelSelectionChangeEvent(this)));
     }
 
-    public void setStage(Stage s){
+    public void setStage(Stage s) {
         this.stage = s;
     }
 
@@ -93,11 +73,11 @@ public class AdminController implements ApplicationListener<ControlPanelSelectio
     }
 
     @FXML
-    private void onClickSet(){
-        if(this.roleChoiceBox.getValue() != null){
-            if(this.usersListView.getSelectionModel().getSelectedItem() != null){
+    private void onClickSet() {
+        if (this.roleChoiceBox.getValue() != null) {
+            if (this.usersListView.getSelectionModel().getSelectedItem() != null) {
                 User u = this.usersListView.getSelectionModel().getSelectedItem();
-                this.userService.getRoleFromName(RoleName.getEnum(this.roleChoiceBox.getValue()).toString()).ifPresent( role ->{
+                this.userService.getRoleFromName(RoleName.getEnum(this.roleChoiceBox.getValue()).toString()).ifPresent(role -> {
                     u.setRole(role);
                     this.userService.updateUser(u);
                     this.usersListView.refresh();
