e70482690ccec9483599b768b06c5648776302af
Mateusz Skowron <405743@student.agh.edu.pl>
2022-12-11 01:14:30 +0100

    Refactor Repositories

--- src/main/java/pl/edu/agh/cs/to/cinemamak/repository/RoleRepository.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/repository/RoleRepository.java
@@ -3,7 +3,6 @@ package pl.edu.agh.cs.to.cinemamak.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.edu.agh.cs.to.cinemamak.model.Role;
-
 import java.util.Optional;
 
 @Repository
