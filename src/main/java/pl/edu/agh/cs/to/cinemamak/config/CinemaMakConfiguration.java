a67f969e9593e3383a8fb4309b6d035bb0afc57e
Karol Wilk <400974@student.agh.edu.pl>
2022-12-23 17:46:12 +0100

    Add rest of UI helper

--- src/main/java/pl/edu/agh/cs/to/cinemamak/config/CinemaMakConfiguration.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/config/CinemaMakConfiguration.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.edu.agh.cs.to.cinemamak.helpers.RoleUIHelper;
+import pl.edu.agh.cs.to.cinemamak.service.SessionService;
 
 @Configuration
 public class CinemaMakConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RoleUIHelper roleUIHelper(SessionService sessionService) {
+        return new RoleUIHelper(sessionService);
     }
 }
