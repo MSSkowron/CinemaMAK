3318a0e9694e1fe8ea6e32687b562b055a622795
Karol Wilk <400974@student.agh.edu.pl>
2022-12-28 19:42:40 +0100

    Expand repository methods

--- src/main/java/pl/edu/agh/cs/to/cinemamak/repository/SeatRepository.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/repository/SeatRepository.java
@@ -2,7 +2,12 @@ package pl.edu.agh.cs.to.cinemamak.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.edu.agh.cs.to.cinemamak.model.Room;
 import pl.edu.agh.cs.to.cinemamak.model.Seat;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+    Collection<Seat> getSeatByRoom(Room room);
 }
--- src/main/java/pl/edu/agh/cs/to/cinemamak/repository/TicketRepository.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/repository/TicketRepository.java
@@ -2,8 +2,13 @@ package pl.edu.agh.cs.to.cinemamak.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.edu.agh.cs.to.cinemamak.model.Performance;
+import pl.edu.agh.cs.to.cinemamak.model.Seat;
 import pl.edu.agh.cs.to.cinemamak.model.Ticket;
+
+import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    Optional<Ticket> findTicketByPerformanceAndSeat(Performance performance, Seat seat);
 }
