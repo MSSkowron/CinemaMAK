90fc844b4d617c2208cc3af4550027960c5fa96f
Mateusz Skowron <405743@student.agh.edu.pl>
2023-01-03 17:12:15 +0100

    Implement EmailService functions

--- src/main/java/pl/edu/agh/cs/to/cinemamak/service/EmailService.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/service/EmailService.java
@@ -4,6 +4,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import pl.edu.agh.cs.to.cinemamak.model.RoleName;
+import pl.edu.agh.cs.to.cinemamak.model.User;
+
+import java.util.Optional;
 
 @Service
 public class EmailService {
@@ -11,57 +14,57 @@ public class EmailService {
     private final SessionService sessionService;
     private final  UserService userService;
 
-    public EmailService(JavaMailSender mailSender, SessionService sessionServicem, UserService userService) {
+    public EmailService(JavaMailSender mailSender, SessionService sessionService, UserService userService) {
         this.mailSender = mailSender;
-        this.sessionService = sessionServicem;
+        this.sessionService = sessionService;
         this.userService = userService;
     }
 
-    public void sendToUser(String to, String subject, String body) {
+    public void sendToUser(User user, String subject, String body) {
+        Optional<User> sender = sessionService.getCurrentUser();
+        if (sender.isEmpty()) {
+            return;
+        }
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
+        message.setTo(user.getEmailAddress());
         message.setSubject(subject);
-        message.setText("Sent from user: " + sessionService.getCurrentUser().get().getEmailAddress() + "\n" + body);
+        message.setText("Sent by: " + sender.get().getEmailAddress() + "\n" + body);
 
         mailSender.send(message);
     }
 
-    public void sendToEmployees(String subject, String body) {
+    public void sendToUsersWithRole(RoleName roleName, String subject, String body) {
+        Optional<User> sender = sessionService.getCurrentUser();
+        if (sender.isEmpty()) {
+            return;
+        }
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setSubject(subject);
-        message.setText("Sent from user: " + sessionService.getCurrentUser().get().getEmailAddress() + "\n" + body);
+        message.setText("Sent from user: " + sender.get().getEmailAddress() + "\n" + body);
 
         userService.getUsers().ifPresent(listU -> listU.forEach(user -> {
-            if (user.getRole().getRoleName().equals(RoleName.Employee)) {
+            if (user.getRole().getRoleName().equals(roleName)) {
                 message.setTo(user.getEmailAddress());
                 mailSender.send(message);
             }
         }));
     }
 
-    public void sendToAdmins(String subject, String body) {
+    public void sendToAllUsers(String subject, String body) {
+        Optional<User> sender = sessionService.getCurrentUser();
+        if (sender.isEmpty()) {
+            return;
+        }
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setSubject(subject);
-        message.setText("Sent from user: " + sessionService.getCurrentUser().get().getEmailAddress() + "\n" + body);
+        message.setText("Sent from user: " + sender.get().getEmailAddress() + "\n" + body);
 
         userService.getUsers().ifPresent(listU -> listU.forEach(user -> {
-            if (user.getRole().getRoleName().equals(RoleName.Admin)) {
-                message.setTo(user.getEmailAddress());
-                mailSender.send(message);
-            }
-        }));
-    }
-
-    public void sendToManagers(String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject(subject);
-        message.setText("Sent from user: " + sessionService.getCurrentUser().get().getEmailAddress() + "\n" + body);
-
-        userService.getUsers().ifPresent(listU -> listU.forEach(user -> {
-            if (user.getRole().getRoleName().equals(RoleName.Manager)) {
-                message.setTo(user.getEmailAddress());
-                mailSender.send(message);
-            }
+            message.setTo(user.getEmailAddress());
+            mailSender.send(message);
         }));
     }
 }
