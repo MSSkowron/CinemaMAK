310df4b757c22f681831a6ee69955731f561e4f5
Ch-Adrian <aderter116@gmail.com>
2023-01-05 22:57:14 +0100

    Implementation: Movie search in add recommendation view.

--- src/main/java/pl/edu/agh/cs/to/cinemamak/controller/RecommendationsFormController.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/controller/RecommendationsFormController.java
@@ -2,15 +2,17 @@ package pl.edu.agh.cs.to.cinemamak.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import pl.edu.agh.cs.to.cinemamak.event.MovieSelectedEvent;
 import pl.edu.agh.cs.to.cinemamak.event.TableRecommendationsChangeEvent;
 import pl.edu.agh.cs.to.cinemamak.model.Movie;
 import pl.edu.agh.cs.to.cinemamak.model.Recommendation;
@@ -23,44 +25,52 @@ import java.util.Optional;
 
 @Component
 @FxmlView("recommendations-form-view.fxml")
-public class RecommendationsFormController {
+public class RecommendationsFormController implements ApplicationListener<MovieSelectedEvent> {
     @FXML
     public ChoiceBox<String> movieChoiceBox;
     @FXML
     public DatePicker dateFromPicker;
     @FXML
     public DatePicker dateToPicker;
+    public TextField textFieldMovie;
+    public Button buttonSearch;
 
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
 
     private final MovieService movieService;
     private final RecommendationService recommendationService;
+    private final FxWeaver fxWeaver;
     private Stage stage;
-
+    private Optional<Recommendation> recommendation = Optional.empty();
+    private Optional<Movie> selectedMovie = Optional.empty();
     public RecommendationsFormController(MovieService movieService,
-                                     RecommendationService recommendationService){
+                                     RecommendationService recommendationService,
+                                         FxWeaver fxWeaver){
         this.movieService = movieService;
         this.recommendationService = recommendationService;
+        this.fxWeaver = fxWeaver;
     }
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
-    public void initialize(){
-        this.movieService.getMovies().ifPresent(list -> list.forEach(movie ->
-                this.movieChoiceBox.getItems().add(movie.getId()+" "+movie.getTitle())));
-    }
+//    public void initialize(){
+//        this.movieService.getMovies().ifPresent(list -> list.forEach(movie ->
+//                this.movieChoiceBox.getItems().add(movie.getId()+" "+movie.getTitle())));
+//    }
 
     public void onActionAdd(){
-        String title = this.movieChoiceBox.getValue();
+//        String title = this.movieChoiceBox.getValue();
+        String title = this.selectedMovie.get().getTitle();
+        Long movieId = this.selectedMovie.get().getId();
         LocalDate dateFrom = this.dateFromPicker.getValue();
         LocalDate dateTo = this.dateToPicker.getValue();
 
         if(dateTo != null && dateFrom != null && title != null){
-            Optional<Movie> movie = movieService.getMovieById(Long.parseLong(title.split("\\s")[0]));
-
+//            Optional<Movie> movie = movieService.getMovieById(Long.parseLong(title.split("\\s")[0]));
+            Optional<Movie> movie = movieService.getMovieById(movieId);
             if(dateTo.isBefore(dateFrom)){
                 showErrorDialog("Error occurred while adding a new recommendation",
                         "Begin date must be before end date.");
@@ -109,6 +119,27 @@ public class RecommendationsFormController {
 
     public void onActionCancel(ActionEvent actionEvent) {
         stage.close();
+    }
+
+    public void onActionSearch(ActionEvent actionEvent) {
+        Stage stageMovieSearch = new Stage();
+        this.selectedMovie = Optional.of(new Movie());
+        fxWeaver.loadController(MovieSearchController.class).setStage(stageMovieSearch);
+        fxWeaver.loadController(MovieSearchController.class).setSelectedMovie(this.selectedMovie.get());
+
+        Scene scene = new Scene(fxWeaver.loadView(MovieSearchController.class));
+        stageMovieSearch.setScene(scene);
+        stageMovieSearch.setTitle("Movie search");
+        stageMovieSearch.initModality(Modality.WINDOW_MODAL);
+        stageMovieSearch.setAlwaysOnTop(true);
+        stageMovieSearch.initOwner(stage);
+        stageMovieSearch.show();
+    }
+
+    @Override
+    public void onApplicationEvent(MovieSelectedEvent event) {
+        if(this.selectedMovie.isEmpty()) return;
+        this.textFieldMovie.setText(this.selectedMovie.get().getTitle());
     }
 
 }
