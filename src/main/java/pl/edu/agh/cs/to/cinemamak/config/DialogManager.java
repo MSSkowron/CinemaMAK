72595905f482863d39d7407acd64ca574b2da270
Ch-Adrian <aderter116@gmail.com>
2023-01-07 15:20:39 +0100

    Fix & Refactor: MovieDetailsController method onButtonDelete
    Fixed problem with associated reccomendations during delete operation.
    	Associated recommendations must be deleted also.
    Refactored confirmation dialog.

--- src/main/java/pl/edu/agh/cs/to/cinemamak/config/DialogManager.java
+++ src/main/java/pl/edu/agh/cs/to/cinemamak/config/DialogManager.java
@@ -1,13 +1,16 @@
 package pl.edu.agh.cs.to.cinemamak.config;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.Window;
+
+import java.util.Optional;
 
 public class DialogManager {
 
     Alert dialogInformation = null;
-
+    Alert dialogConfirmation = null;
     public DialogManager(){}
 
     private void createAlertInformation(){
@@ -16,14 +19,34 @@ public class DialogManager {
         this.dialogInformation.setTitle("Information");
     }
 
-    public void setDialogInformation(Window owner, String headerText,
+    private void createAlertConfirmation(){
+        this.dialogConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        this.dialogConfirmation.initModality(Modality.APPLICATION_MODAL);
+        this.dialogConfirmation.setTitle("Confirmation");
+    }
+
+    public void showInformation(Window owner, String headerText, String contentText,
                                      javafx.event.EventHandler<javafx.scene.control.DialogEvent> eventHandler){
-        if(this.dialogInformation == null){
-            this.createAlertInformation();
-        }
+
+        this.createAlertInformation();
+
         this.dialogInformation.initOwner(owner);
         this.dialogInformation.setHeaderText(headerText);
+        this.dialogInformation.setContentText(contentText);
         this.dialogInformation.setOnCloseRequest(eventHandler);
         this.dialogInformation.show();
+
+    }
+
+    public boolean askForConfirmation(Window owner, String headerText, String contentText){
+
+        this.createAlertConfirmation();
+
+        this.dialogConfirmation.initOwner(owner);
+        this.dialogConfirmation.setHeaderText(headerText);
+        this.dialogConfirmation.setContentText(contentText);
+        Optional<ButtonType> result = this.dialogConfirmation.showAndWait();
+
+        return result.filter(buttonType -> buttonType == ButtonType.OK).isPresent();
     }
 }
